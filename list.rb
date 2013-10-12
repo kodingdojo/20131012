267ebe60beeddaa77e70bad1b8fcc5ace93e3bf9
Trung Ngo <ndtrung4419@gmail.com>
2013-10-12 16:48:22 +0700

    chin

--- list.rb
+++ list.rb
@@ -2,6 +2,10 @@ require 'minitest/autorun'
 
 class SingleList
     attr_accessor :current, :next
+
+    def initialize(current = nil)
+        @current = current
+    end
 end
 
 
@@ -11,10 +15,16 @@ class MyTest < Minitest::Test
     end
 end
 
+
 class TestSingleListDefinition < Minitest::Test
     def test_initialize_without_arguments
         l = SingleList.new
         assert_equal nil, l.current
         assert_equal nil, l.next
+    end
+
+    def test_initialize_with_current
+        l = SingleList.new("head")
+        assert_equal "head", l.current
     end
 end
