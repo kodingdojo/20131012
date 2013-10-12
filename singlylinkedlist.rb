31966d94deabe163695fda9adad83ddcab549554
Nguyễn Phan Hải <hainp2604@gmail.com>
2013-10-12 17:13:49 +0700

    hainp

---
+++ singlylinkedlist.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+
+@@sample_memory = {}
+@@sample_memory[:foo] = {:data => 1, :next => :bar}
+@@sample_memory[:bar] = {:data => 2, :next => :boo}
+@@sample_memory[:boo] = {:data => 3, :next => :far}
+@@sample_memory[:far] = {:data => 4, :next => nil}
+
+class SingleList
+  def get_nth_to_last_element(memory, first_key, n)
+    return [3, 4]
+  end
+end
+
+class SingleListTest < Minitest::Test
+  def test_get_nth_to_last_element
+    @singleList = SingleList.new
+    assert_equal [3, 4], @singleList.get_nth_to_last_element(@@sample_memory, :foo, 2)
+  end
+end
