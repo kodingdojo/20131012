f9b09933a7b98f08a0e0a3f4a57fa167fa004342
Trung Ngo <ndtrung4419@gmail.com>
2013-10-12 16:42:32 +0700

    chin

--- list.rb
+++ list.rb
@@ -1,7 +1,20 @@
 require 'minitest/autorun'
 
+class SingleList
+    attr_accessor :current, :next
+end
+
+
 class MyTest < Minitest::Test
     def test_aoe
         assert_equal 1, 1
+    end
+end
+
+class TestSingleListDefinition < Minitest::Test
+    def test_initialize
+        l = SingleList.new
+        assert_equal nil, l.current
+        assert_equal nil, l.next
     end
 end
