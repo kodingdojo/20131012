bfd89b2ad98b101e960013c19e2a2475274c7af0
Nguyễn Phan Hải <hainp2604@gmail.com>
2013-10-12 17:40:39 +0700

    hainp

--- singlylinkedlist.rb
+++ singlylinkedlist.rb
@@ -8,10 +8,16 @@ require 'minitest/autorun'
 
 class SingleList
   def get_nth_to_last_element(memory, first_key, n)
-    if n == 2
-      [3, 4]
+    if n == 4
+      []
+    elsif n == 3
+      [4].concat(get_nth_to_last_element(memory, :foo, 4))
+    elsif n == 2
+      [3].concat(get_nth_to_last_element(memory, :foo, 3))
+    elsif n == 1
+      [2].concat(get_nth_to_last_element(memory, :foo, 2))
     else
-      [2, 3, 4]
+      [1].concat(get_nth_to_last_element(memory, :foo, 1))
     end
   end
 end
@@ -20,8 +26,20 @@ class SingleListTest < Minitest::Test
   def test_get_nth_to_last_element
     @singleList = SingleList.new
 
+    assert_equal [2].concat(@singleList.get_nth_to_last_element(@@sample_memory, :foo, 2)),
+                 @singleList.get_nth_to_last_element(@@sample_memory, :foo, 1)
+  
+    assert_equal [1, 2, 3, 4],
+                 @singleList.get_nth_to_last_element(@@sample_memory, :foo, 0)
+    
+    assert_equal [2, 3, 4],
+                 @singleList.get_nth_to_last_element(@@sample_memory, :foo, 1)
+  
     assert_equal [3, 4], @singleList.get_nth_to_last_element(@@sample_memory, :foo, 2)
 
-    assert_equal [2, 3, 4], @singleList.get_nth_to_last_element(@@sample_memory, :foo, 1)
+    assert_equal [4], @singleList.get_nth_to_last_element(@@sample_memory, :foo, 3)
+
+    assert_equal [], @singleList.get_nth_to_last_element(@@sample_memory, :foo, 4)
+
   end
 end
